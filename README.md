602528f4148e984d94682056ad7f0623487304ec
Nicholas Glenn <nick@helpfulhuman.com>
2017-11-23 23:13:10 -0800

    Working on some new updates to createForm around validation

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Rehance-Forms
 
+This library offers utilities for creating forms in React using stateless components.
+
 ## Getting Started
 
 Install via `npm`:
@@ -8,27 +10,51 @@ Install via `npm`:
 npm i -S rehance-forms
 ```
 
-## Usage
+## `createForm`
+
+The `createForm` method allows you to configure the fields of your form, how to handle submit, etc...  Each defined field will have methods generated automatically on construction that are then passed down via props to the nested component.
+
+Let's kick off with an example.  Here we have a simple form with a text input named `myNumber`.  The value of this input field gets converted to an integer via the given `format()` method before being passed to the `validate()` method.  We can then access the field data and methods on the "namespace" key (which is `form` by default) of the props passed to the nested component.
+
+For our `myNumber` field, we can see that an `onMyNumberChange` method is created and we can access the new field value using `myNumber.value`.
 
 ```tsx
 import * as React from "react";
 import {createForm} from "rehance-forms";
 
-const LoginForm = createForm({
+const NumericInput = createForm({
   fields: {
-    email: "",
-    password: "",
+    myNumber: {
+      default: 0,
+      format: (numString) => parseInt(numString),
+      validate: (num) => (num < 1 || num > 10 ? "Must be between 1 and 10." : null),
+    },
   },
-  onSubmit(fields, props) {
-    console.log(fields.email, fields.password);
+  onSubmit(props) {
+    alert(`You entered ${props.form.myNumber.value}`);
   },
 })(function (props) {
+  var form = props.form;
+  var errors = null;
+
+  if (form.errors.length > 0) {
+    errors = (<span className="error">{form.errors[0]}></span>);
+  }
+
   return (
     <div>
-      <input type="email" onChange={props.onEmailChange} value={props.email} />
-      <input type="password" onChange={props.onPasswordChange} value={props.password} />
-      <button onClick={props.onSubmit}>Log In</button>
+      {errors}
+      <input type="numeric" onChange={form.onMyNumberChange} value={form.myNumber.value} />
+      <button disabled={!form.isValid} onClick={form.onSubmit}>Log In</button>
     </div>
   );
 });
 ```
+
+#### Field State Data
+
+Key | Type | Description
+----|------|------------
+**value** | `any` | The current value for the field.
+**error** | `string` | The error messages for the field, or `null` if there are no errors.
+**dirty** | `boolean` | Set to `true` when the user has interacted with the field.
--- src/createForm.tsx
+++ src/createForm.tsx
@@ -1,30 +1,61 @@
 import * as React from "react";
 
-export interface FormOptions<Fields, Props> {
-  fields: Fields | {(props: Props): Fields};
-  onSubmit?(fields: Fields, props: Props): void;
-  updateFieldsFromProps?(Props): Fields;
+export interface FieldOptions {
+  default?: any;
+  format?(val: any): any;
+  validate?(val: any): any;
 }
 
-export interface ComponentFactory<Fields, Props, ChildProps> {
+export interface Fields {
+  [key: string]: FieldOptions;
+}
+
+export interface FormOptions<Props, ChildProps> {
+  fields: Fields | {(props: Props): Fields};
+  namespace?: string;
+  onSubmit?(props: ChildProps): void;
+  updateFieldsFromProps?(props: ChildProps): Fields;
+}
+
+export interface ComponentFactory<Props, ChildProps> {
   (Component: React.ComponentClass<ChildProps>|React.StatelessComponent<ChildProps>): React.ComponentClass<Props>;
 }
 
-export interface EnhancedComponent<Fields, Props> extends React.PureComponent<Props> {
+export interface EnhancedComponent<Props> extends React.PureComponent<Props> {
   methods: object;
+}
+
+interface State {
+  [name: string]: {
+    value: any,
+    error: string,
+    dirty: boolean,
+  };
+}
+
+function isFunc(val: any): boolean {
+  return (typeof val === "function");
 }
 
 function capitalize(str: string): string {
   return (str.charAt(0).toUpperCase() + str.slice(1));
 }
 
-export function createForm<Fields = any, Props = any, ChildProps = any>(opts: FormOptions<Fields, Props>): ComponentFactory<Fields, Props, ChildProps> {
+function _format(val: any): any {
+  return val;
+}
+
+function _validate(): void {}
+
+export function createForm<Props = any, ChildProps = any>(opts: FormOptions<Props, ChildProps>): ComponentFactory<Props, ChildProps> {
+  // Cache options and apply defaults
+  var ns = (opts.namespace || "form");
+
+  // Return a factory method for generating new components
   return function (Component) {
-    return class extends React.Component<Props> implements EnhancedComponent<Fields, Props> {
+    return class extends React.Component<Props, State> implements EnhancedComponent<Props> {
 
-      state: object = {};
-
-      fieldNames: string[] = [];
+      state: State = {};
 
       methods: object = {};
 
@@ -33,54 +64,103 @@ export function createForm<Fields = any, Props = any, ChildProps = any>(opts: Fo
         this.handleSubmit = this.handleSubmit.bind(this);
 
         // Get the defined fields
-        var fields = opts.fields;
-        if (typeof fields === "function") {
+        var fields = opts.fields as any;
+        if (isFunc(fields)) {
           fields = fields(props);
         }
 
         // Create the methods for updating field states
-        for (var field in fields as any) {
-          this.fieldNames.push(field);
-          this.state[field] = opts.fields[field];
-          this.bindChangeHandler(field);
+        for (var fieldName in fields) {
+          this.setupField(fieldName, fields[fieldName]);
         }
       }
 
-      bindChangeHandler(fieldName: string) {
-        var methodName = "on" + capitalize(fieldName) + "Change";
-        this.methods[methodName] = (val) => {
-          // If we're dealing with a raw Element event, then grab the value from the element
-          // automagically, otherwise we'll take what's given.
-          if (val.currentTarget && val.currentTarget.value) {
-            val = val.currentTarget.value;
+      setupField(name: string, field: FieldOptions): void {
+
+        // Capitalize the name for method names
+        const Name = capitalize(name);
+
+        // Get a formatter and validator for the field
+        const format = (isFunc(field.format) ? field.format : _format);
+        const validate = (isFunc(field.validate) ? field.validate : _validate);
+
+        // Create a method that will generate a new field state based on a new value
+        const getUpdatedState = (value: any, dirty: boolean) => {
+          // Format and validate the value
+          var error = null;
+          try {
+            value = format(value);
+            error = validate(value);
+          } catch (err) {
+            error = err.message;
           }
 
-          // Update the component's field state
-          this.setState({ [fieldName]: val });
+          // Return the new state for the field
+          return { value, error, dirty };
         };
+
+        // Bind a set method handler for this field
+        const set = this.methods[`set${Name}`] = (value) => {
+          // Update the component's field state
+          this.setState({ [name]: getUpdatedState(value, true) });
+        };
+
+        // Bind the onChange handler for this field
+        this.methods[`on${Name}Change`] = (value) => {
+          // If value is an event, get the value of the DOM element
+          if (value.currentTarget && value.currentTarget.value) {
+            value = value.currentTarget.value;
+          }
+
+          set(value);
+        };
+
+        // Set the default state for the field
+        this.state[name] = getUpdatedState(field.default, false);
+
       }
 
       componentWillReceiveProps(nextProps: Props) {
-        if (typeof opts.updateFieldsFromProps === "function") {
-          this.setState( opts.updateFieldsFromProps(nextProps) );
+        if (isFunc(opts.updateFieldsFromProps)) {
+          // this.setState( opts.updateFieldsFromProps(nextProps) );
         }
       }
 
       handleSubmit(): void {
-        if (typeof opts.onSubmit === "function") {
-          opts.onSubmit(this.state as any, this.props);
+        if (isFunc(opts.onSubmit)) {
+          opts.onSubmit(this.getChildProps());
         }
       }
 
+      getChildProps(): ChildProps {
+        var errors = [];
+        var isValid = true;
+        for (var key in this.state) {
+          if (this.state[key].error) {
+            isValid = false;
+            errors.push(this.state[key].error);
+          }
+        }
+
+        return {
+          ...this.props as any,
+          [ns]: {
+            onSubmit: this.handleSubmit,
+            errors: errors,
+            isValid: isValid,
+            ...this.state,
+            ...this.methods,
+          }
+        };
+      }
+
       render() {
-        var { children, ...props } = this.props as any;
+        var childProps = this.getChildProps();
+
         return (
           <Component
-            onSubmit={this.handleSubmit}
-            {...this.methods}
-            {...this.state}
-            {...props}>
-            {children}
+            {...childProps}>
+            {this.props.children}
           </Component>
         );
       }
