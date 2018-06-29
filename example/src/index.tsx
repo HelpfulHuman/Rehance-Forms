d47e59d4440923f7a41bab9df6fe399b1128d53f
Nicholas Glenn <nick@helpfulhuman.com>
2018-06-28 17:32:32 -0700

    Started updating examples (might replace with Storybook)

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,81 +1,59 @@
 import * as React from "react";
-import {render} from "react-dom";
-import {createForm, withYup} from "./stateside";
-import * as yup from "yup";
+import { render } from "react-dom";
+import { Form, Input, Select, ErrorOutput, SubmitButton, FormOutput, Subscriber, toNumber } from "./lib";
 
-function renderError(error, i) {
-  return (
-    <li key={i}>{error}</li>
-  );
-}
-
-function FormLayout({ form }) {
-  return (
-    <div>
-      <div>
-        <input type="email" {...form.email} />
-        <span>{form.email.error}</span>
-      </div>
-      <div>
-        <input type="password" {...form.password} />
-        <span>{form.password.error}</span>
-      </div>
-      <button disabled={!form.isValid} onClick={form.onSubmit}>Submit</button>
-    </div>
-  );
-}
-
-const schema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
-});
-
-// interface FormFields {
-//   email: string;
-//   password: string;
-// }
-
-const Form = createForm<any, any>({
-  fields: {
-    email: "",
-    password: "",
-  },
-  validate: withYup(schema),
-  onSubmit(fields, props) {
-    props.setResult(fields);
-  },
-})(FormLayout);
-
-class App extends React.Component {
-  state = { result: null };
-
-  constructor(props, context) {
-    super(props, context);
-    this.setResult = this.setResult.bind(this);
-  }
-
-  setResult(result: any): void {
-    this.setState({ result });
-  }
-
-  render() {
-    var result = this.state.result;
-    var sentMessage;
-
-    if (result !== null) {
-      sentMessage = `Sent: ${JSON.stringify(result)}`;
-    }
-
-    return (
-      <div>
-        {sentMessage}
-        <Form setResult={this.setResult} />
-      </div>
-    );
-  }
+function handleFormSubmit({ values }: FormOutput) {
+  console.log(values);
 }
 
 render(
-  <App />
+  <LoginForm />
   , document.getElementById("root")
 );
+
+function LoginForm() {
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <div>
+        <Input type="email" name="email" required placeholder="Email" />
+        <ErrorOutput name="email" className="Errors" />
+      </div>
+      <div>
+        <Input type="password" name="password" required min={8} max={24} placeholder="Password" />
+        <ErrorOutput name="password" />
+      </div>
+      <SubmitButton>
+        Login
+      </SubmitButton>
+    </Form>
+  );
+}
+
+function LargerForm() {
+  return (
+    <Form onSubmit={handleFormSubmit}>
+      <div>
+        <Input name="firstName" />
+        <Input name="lastName" />
+
+        <Subscriber name={["firstName", "lastName"]}>
+          {({ getValue }) => <span>Full Name: {getValue("firstName")} {getValue("lastName")}</span>}
+        </Subscriber>
+      </div>
+      <div>
+        <Input type="email" name="email" required validateOnChange />
+        <ErrorOutput name="email" />
+      </div>
+      <div>
+        <Select name="favColor" options={[
+          { value: "Green", label: "Green" },
+          { value: "Blue", label: "Blue" },
+          { value: "Red", label: "Red" },
+        ]} />
+      </div>
+      <SubmitButton className="Button">
+        Submit
+    </SubmitButton>
+    </Form>
+  );
+}
