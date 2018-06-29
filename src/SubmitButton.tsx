e0dbd580731ae0768bba6a0768d788b5cef45a83
Nicholas Glenn <nick@helpfulhuman.com>
2018-06-28 17:29:58 -0700

    Added initial concept for a SubmitButton component

---
+++ src/SubmitButton.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { withForm, WithContextProps } from "./helpers";
+import { Omit } from "./types";
+import { FormContext } from "./Context";
+
+export type SubmitButtonProps =
+  & Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "type" | "disabled">
+  & {
+    disabled?(form: FormContext): boolean;
+  };
+
+class _SubmitButton extends React.PureComponent<WithContextProps<SubmitButtonProps>> {
+
+  static defaultProps: Partial<SubmitButtonProps> = {
+    disabled() { return false; },
+  };
+
+  unsubFormUpdate: Function;
+  unsubFieldUpdate: Function;
+
+  componentWillMount() {
+    this.unsubFormUpdate = this.props.form.onFormUpdate(() => this.forceUpdate());
+    this.unsubFieldUpdate = this.props.form.onFieldUpdate(() => this.forceUpdate());
+  }
+
+  componentWillUnmount() {
+    this.unsubFormUpdate();
+    this.unsubFieldUpdate();
+  }
+
+  render() {
+    const { form, disabled, children, ...props } = this.props;
+
+    return (
+      <button
+        {...props}
+        type="submit"
+        disabled={disabled!(form)}
+      >
+        {children}
+      </button>
+    );
+  }
+
+}
+
+export const SubmitButton = withForm<SubmitButtonProps>(_SubmitButton as any);
