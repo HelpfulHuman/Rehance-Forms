e0afa2eb7407c9300b3927299770d7269c47cf33
Nicholas Glenn <nick@helpfulhuman.com>
2018-08-28 22:05:25 -0700

    Removing Toggle examples from the MultiFieldSubscriber example

--- example/src/forms/MultiFieldSubscriber.tsx
+++ example/src/forms/MultiFieldSubscriber.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Form, Input, Subscriber, ErrorOutput, Select, SubmitButton, Toggle } from "rehance-forms";
+import { Form, Input, Subscriber, SubmitButton } from "rehance-forms";
 import { FormWrapper } from "./FormWrapper";
 
 export class MultiFieldSubscriber extends React.PureComponent {
@@ -20,18 +20,6 @@ export class MultiFieldSubscriber extends React.PureComponent {
           <div>
             <Input name="firstName" />
             <Input name="lastName" />
-          </div>
-          <div>
-            Is there support for toggle components?
-            <Toggle name="exampleToggle" />
-          </div>
-          <div>
-            How about custom toggle components?
-            <Toggle name="exampleToggle">
-              {({ value, onToggle }) => (
-                <span onClick={onToggle}>{value ? "Yes" : " No"}</span>
-              )}
-            </Toggle>
           </div>
           <SubmitButton className="Button">
             Submit
